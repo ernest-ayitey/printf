f2b9b4d67b0bfa00ff7b417db85c8fe38dd31bc2
ernest-ayitey <ernestayitey65@gmail.com>
2023-07-26 00:01:11 +0000

    team work

--- functions.c
+++ functions.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_char - character is printed.
- * @types:  arguments types
+ * print_char - Prints a chararacter
+ * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags:  determines the active flags
+ * @flags:  dispaly active flags
  * @width: Width
  * @precision: Precision specification
  * @size: Size specifier
@@ -17,6 +17,7 @@ int print_char(va_list types, char buffer[],
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
+
 /**
  * print_string - Prints a string
  * @types: List a of arguments
@@ -72,7 +73,6 @@ int print_string(va_list types, char buffer[],
 	return (write(1, str, length));
 }
 
-
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
@@ -110,20 +110,20 @@ int print_int(va_list types, char buffer[],
 {
 	int i = BUFF_SIZE - 2;
 	int is_negative = 0;
-	long int k = va_arg(types, long int);
+	long int n = va_arg(types, long int);
 	unsigned long int num;
 
-	k  = convert_size_number(n, size);
+	n = convert_size_number(n, size);
 
-	if (k == 0)
+	if (n == 0)
 		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
-	num = (unsigned long int)k;
+	num = (unsigned long int)n;
 
-	if (k < 0)
+	if (n < 0)
 	{
-		num = (unsigned long int)((-1) * k);
+		num = (unsigned long int)((-1) * n);
 		is_negative = 1;
 	}
 
@@ -138,6 +138,7 @@ int print_int(va_list types, char buffer[],
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
+/
 /**
  * print_binary - Prints an unsigned number
  * @types: Lista of arguments
@@ -175,7 +176,7 @@ int print_binary(va_list types, char buffer[],
 		sum += a[i];
 		if (sum || i == 31)
 		{
-			char d = '0' + a[i];
+			char z = '0' + a[i];
 
 			write(1, &z, 1);
 			count++;
@@ -183,4 +184,3 @@ int print_binary(va_list types, char buffer[],
 	}
 	return (count);
 }
-
